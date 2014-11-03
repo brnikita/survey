bd4fcbcc9963cb8c2aab10516ed7934d802b5213
brnikita <nikita-bragin@mail.ru>
2014-11-03 23:06:44 +0300

    final screen v1 completed

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.js-player').jPlayer({
+        ready: function () {
+            $(this).jPlayer('setMedia', {
+                mp3: '/audio/uk.mp3'
+            });
+        },
+        swfPath: '/js/vendors/jplayer',
+        supplied: 'mp3',
+        cssSelectorAncestor: '',
+        cssSelector: {
+            play: '.js-play',
+            pause: '.js-pause',
+            playBar: '.js-play-bar',
+            noSolution: '.js-no-solution'
+        }
+    });
+});
