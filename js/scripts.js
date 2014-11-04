b88c5357133e7c70006ce3d0770f6759aa7c5ac1
brnikita <nikita-bragin@mail.ru>
2014-11-04 16:04:23 +0300

    Step handler added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
-$(function(){
+$(function () {
+    var currentStep = 0,
+        $loaderScreen = $('.js-loading-screen'),
+        $finalPage = $('.js-final-page'),
+        steps = [
+            $('.js-start-page'),
+            $('.question-page-1'),
+            $('.question-page-2'),
+            $('.question-page-3'),
+            $('.question-page-4')
+        ];
+
+    /**
+     * Function shows loader page and start timer
+     *
+     * @function
+     * @name showLoaderScreen
+     * @param {number} loadTime
+     * @returns {undefined}
+     */
+    function showLoaderScreen(loadTime) {
+        $loaderScreen.removeClass('hide');
+        setTimeout(function () {
+            $loaderScreen.addClass('hide');
+            $finalPage.removeClass('hide');
+        }, loadTime);
+    }
+
     $('.js-player').jPlayer({
         ready: function () {
-//            $(this).jPlayer('setMedia', {
-//                mp3: '/audio/uk.mp3'
-//            }).jPlayer('play');
+            $(this).jPlayer('setMedia', {
+                mp3: '/audio/uk.mp3'
+            }).jPlayer('play');
         },
         swfPath: '/js/vendors/jplayer',
         supplied: 'mp3',
@@ -14,5 +41,24 @@ $(function(){
             playBar: '.js-play-bar',
             noSolution: '.js-no-solution'
         }
+    });
+
+    $('.js-next').on('click', function (event) {
+        event.preventDefault();
+        steps[currentStep].addClass('hide');
+        currentStep++;
+        if (currentStep !== steps.length) {
+            steps[currentStep].removeClass('hide');
+            return;
+        }
+
+        showLoaderScreen(3000);
+    });
+
+    $('.js-previous').on('click', function (event) {
+        event.preventDefault();
+        steps[currentStep].addClass('hide');
+        currentStep--;
+        steps[currentStep].removeClass('hide');
     });
 });
